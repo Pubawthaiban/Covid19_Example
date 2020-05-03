c48c699a73c3b78e2f19ff1ba1016d362ee819d3
Pubawthaiban <champion.w@hotmail.com>
2020-05-03 16:02:28 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+
+import { fatchData } from "./api";
+
+export class App extends Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const fetchData = await fatchData();
+    this.setState({ data: fetchData });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchData = await fatchData(country);
+    this.setState({ data: fetchData, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
+import cx from "classnames";
+import styles from "./Cards.module.css";
+
+const Cards = (props) => {
+  const {
+    data: { confirmed, recovered, deaths, lastUpdate },
+  } = props;
+  console.log(props);
+  if (!confirmed) {
+    return "Loadding...";
+  }
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.infected)}
+        >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              ติดเชื้อแล้ว (Infected)
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">จำนวนผู้ติดเชื้อ COVID-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.recovered)}
+        >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              หายแล้ว (Recovered)
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={recovered.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">จำนวนกรณีการหายจาก COVID-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.deaths)}
+        >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              เสียชีวิต (Deaths)
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={deaths.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">
+              จำนวนผู้เสียชีวิตจาก COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Cards;
